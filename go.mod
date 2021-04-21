30cc84e3739c3fd794c0aabf2611051c020706d3
James Strachan <james.strachan@gmail.com>
2021-04-21 16:29:43 +0100

    fix: add env var parsing

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/jenkins-x-quickstarts/go-chaos
+
+require (
+	github.com/sethvargo/go-envconfig v0.3.2
+)
+
+
+go 1.15
+
--- main.go
+++ main.go
@@ -1,12 +1,38 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"github.com/sethvargo/go-envconfig"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
+type Options struct {
+	// Port the port to lisetn to
+	Port string `env:"PORT,default=8080"`
+}
+
+
+func main() {
+	o := &Options{}
+	ctx := context.TODO()
+	err := envconfig.Process(ctx, o)
+	if err != nil {
+		fmt.Printf("failed: %v\n", err)
+		os.Exit(1)
+		return
+	}
+
+	http.HandleFunc("/", o.handler)
+
+	fmt.Printf("listening to port %s\n", o.Port)
+	http.ListenAndServe(":" + o.Port, nil)
+}
+
+
+func (o*Options) handler(w http.ResponseWriter, r *http.Request) {
 	title := "Jenkins X golang http example"
 
 	from := ""
@@ -20,7 +46,3 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello from:  "+title+"\n")
 }
 
-func main() {
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
-}
